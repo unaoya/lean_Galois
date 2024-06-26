ab807cce23acb4f36df8e2eded2a8eb43b2bc9c9
unaoya <umezakinaoya@gmail.com>
2024-06-27 00:39:44 +0900

    first commit

---
+++ lakefile.lean
@@ -0,0 +1,22 @@
+import Lake
+open Lake DSL
+
+package «lean_Galois» where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «LeanGalois» where
+  -- add any library configuration options here
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
